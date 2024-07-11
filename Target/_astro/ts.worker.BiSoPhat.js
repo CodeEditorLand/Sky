807b76bd1e3620298c1af8090b6bb3647c13440e
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-12 02:12:58 +0300

    squash!
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- Target/_astro/ts.worker.BiSoPhat.js
+++ Target/_astro/ts.worker.BiSoPhat.js
@@ -1,1 +1,5 @@
-function e(e){return new Worker("/_astro/ts.worker-BBBnYv8q.js",{name:e?.name})}export{e as default};
+function e(r) {
+	return new Worker("/_astro/ts.worker-BBBnYv8q.js", { name: r?.name });
+}
+export { e as default };
+//# sourceMappingURL=ts.worker.BiSoPhat.js.map

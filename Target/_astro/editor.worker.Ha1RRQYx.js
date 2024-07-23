a3bf444b624c90660dae6749fbddf80100379dc2
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-23 16:41:32 +0300

    s
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- Target/_astro/editor.worker.Ha1RRQYx.js
+++ Target/_astro/editor.worker.Ha1RRQYx.js
@@ -1,1 +1,5 @@
-function e(e){return new Worker("/_astro/editor.worker-C8ykIlSb.js",{name:e?.name})}export{e as default};
+function e(r) {
+	return new Worker("/_astro/editor.worker-C8ykIlSb.js", { name: r?.name });
+}
+export { e as default };
+//# sourceMappingURL=editor.worker.Ha1RRQYx.js.map

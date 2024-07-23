4d2de8a1cc28652b2e89c7cacaaccb604b541625
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-23 16:42:02 +0300

    s
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- Target/_astro/css.worker.krus72yV.js
+++ Target/_astro/css.worker.krus72yV.js
@@ -1,5 +1,1 @@
-function e(r) {
-	return new Worker("/_astro/css.worker-CrwcPpw_.js", { name: r?.name });
-}
-export { e as default };
-//# sourceMappingURL=css.worker.krus72yV.js.map
+function e(e){return new Worker("/_astro/css.worker-CrwcPpw_.js",{name:e?.name})}export{e as default};
--- Target/_astro/editor.worker.Ha1RRQYx.js
+++ Target/_astro/editor.worker.Ha1RRQYx.js
@@ -1,5 +1,1 @@
-function e(r) {
-	return new Worker("/_astro/editor.worker-C8ykIlSb.js", { name: r?.name });
-}
-export { e as default };
-//# sourceMappingURL=editor.worker.Ha1RRQYx.js.map
+function e(e){return new Worker("/_astro/editor.worker-C8ykIlSb.js",{name:e?.name})}export{e as default};
